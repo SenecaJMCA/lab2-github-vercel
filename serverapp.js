60fe345c7e0d2bacd6e204b5cc3b55ebdf114714
Jairo Calderon <j.calderoneng@gmail.com>
2025-10-17 12:04:44 -0400

    fixing functions in the serverapp.js file

--- serverapp.js
+++ serverapp.js
@@ -1,11 +1,14 @@
 const HTTP_PORT = process.env.PORT || 3000;
 
-const express = requires("express");
+const express = require("express");
 
 const app = express();
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
   res.send(`Jairo Calderon WEB322 ${new Date().toISOString()}`);
+});
+app.get("/{*splat}", (req, res) => {
+  res.spend("Incorrect URL, please try again!");
 });
 
 const server = app.listen(HTTP_PORT, () => {
